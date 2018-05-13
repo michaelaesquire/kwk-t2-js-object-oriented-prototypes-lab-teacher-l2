4c079d8df849456fc3e8f36d16f19e7e0e733f5c
Michaela Olson <mco44@cornell.edu>
2018-05-13 18:10:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
    this.homeState = homeState
    this.training = training
 //
-//   this.veto = function(){
-//     return 'No, I must disagree'
-//   }
+   this.veto = function(){
+     return 'No, I must disagree'
+   }
 //   this.approve = function(){
 //     return 'You can do that!'
 //   }

4c1d40e886b7a56a75fe4a140ddad9c2ed3f421b
Michaela Olson <mco44@cornell.edu>
2018-05-13 18:12:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,6 @@
    this.homeState = homeState
    this.training = training
 
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
-//
 //   this.sayHi = function(){
 //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 //   }
@@ -27,3 +23,8 @@ BoardMember.prototype.doCharity = function(){
 BoardMember.prototype.releasePressStatement = function(){
    return "You will see great things from Scuber."
 }
+
+BoardMember.prototype.sayHi = function(){
+   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
+
